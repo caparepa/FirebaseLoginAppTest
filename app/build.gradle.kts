fa9e745dbb01cfa4a1e4332e917b8c9964c10bcf
C. Serrano <serrano.cjm@gmail.com>
2021-02-11 15:17:57 -0400

    add gradle dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,13 +38,68 @@ android {
 }
 
 dependencies {
-
+    //Core dependencies
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.4.21")
     implementation("androidx.core:core-ktx:1.3.2")
     implementation("androidx.appcompat:appcompat:1.2.0")
-    implementation("com.google.android.material:material:1.3.0")
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+
+    //Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.8")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.8")
+
+    //layouts
     implementation("androidx.constraintlayout:constraintlayout:2.0.4")
+
+    //navigation components
+    implementation("androidx.navigation:navigation-fragment-ktx:2.3.3")
+    implementation("androidx.navigation:navigation-ui-ktx:2.3.3")
+
+    //viewpager2
+    implementation("androidx.viewpager2:viewpager2:1.0.0")
+    implementation("com.google.android.material:material:1.3.0")
+
+    //paging
+    implementation("androidx.paging:paging-runtime:2.1.2")
+}
+
+dependencies {
+    //Test dependencies
     testImplementation("junit:junit:4.13.1")
     androidTestImplementation("androidx.test.ext:junit:1.1.2")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
+}
+
+dependencies {
+    //Third-party dependencies
+
+    //OKHTTP
+    implementation("com.squareup.okhttp3:okhttp:4.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.5.0")
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.jakewharton.retrofit:retrofit2-kotlin-coroutines-adapter:0.9.2")
+
+    //threeten
+    implementation("com.jakewharton.threetenabp:threetenabp:1.2.4")
+
+    //Dependency injection
+    implementation("org.koin:koin-android:2.0.1")
+    implementation("org.koin:koin-androidx-scope:2.0.1")
+    implementation("org.koin:koin-androidx-viewmodel:2.0.1")
+
+    //room
+    implementation("androidx.room:room-ktx:2.2.6")
+    annotationProcessor("androidx.room:room-compiler:2.2.6")
+
+    //GLIDE
+    implementation("com.github.bumptech.glide:glide:4.11.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.10.0")
+
+    //loading
+    implementation("com.github.ybq:Android-SpinKit:1.4.0")
+
 }
